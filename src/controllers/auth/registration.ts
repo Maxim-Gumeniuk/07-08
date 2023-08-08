050a6ce35167f38271ad5c2b99ce4298d2892893
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-08-08 20:22:55 +0300

    fix status

--- src/controllers/auth/registration.ts
+++ src/controllers/auth/registration.ts
@@ -18,7 +18,7 @@ const activate = async (req: Request, res: Response) => {
     const user = await UserModel.findOne({ where: { activationToken } });
 
     if (!user) {
-        res.sendStatus(201);
+        res.sendStatus(Statuses.Not_found);
         return;
     }
 
