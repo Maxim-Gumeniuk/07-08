ca56ce2ff5b6b6695abc6ded7b3bada5c2cd2305
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-08-08 10:16:51 +0300

    add sign_up req

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import { sequelize as conection } from "@/db/connections/user";
 import { registrationRoute } from "@/routes/auth/registration";
+import cors from "cors";
 import "dotenv/config";
 
+const { PORT = 5010, CORS_ORIGIN } = process.env;
 const app = express();
 
+app.use(
+    cors({
+        origin: CORS_ORIGIN,
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(registrationRoute);
-
-const { PORT = 5010 } = process.env;
 
 const start = async (): Promise<void> => {
     try {
